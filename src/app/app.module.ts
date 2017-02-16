a244d650842076a1ac98136d5dc5550dfc3327d1
SirSalute <acetequila@gmail.com>
2017-02-16 03:04:46 +0000

    Criado save de registros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 import { MyApp } from './app.component';
 
 
 import { Page1 } from '../pages/page1/page1';
 import { Registros } from '../pages/registros/registros';
 import { CriarRegistros } from '../pages/criar-registros/criar-registros';
+
+/*export function provideStorage() {
+  return new Storage(['sqlite', 'websql', 'indexeddb'], { name: '__mydb' }// optional config);
+}*/
 
 @NgModule({
   declarations: [
@@ -24,6 +29,14 @@ import { CriarRegistros } from '../pages/criar-registros/criar-registros';
     Registros,
     CriarRegistros
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+  providers: [
+  {
+    provide: ErrorHandler,
+    useClass: IonicErrorHandler,
+    //provide: Storage,
+    //useFactory: provideStorage
+  },
+  Storage
+  ]
 })
 export class AppModule {}
--- src/pages/criar-registros/criar-registros.ts
+++ src/pages/criar-registros/criar-registros.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { registroModel } from './registro-model';
+import { Storage } from '@ionic/storage';
+//import { registroModel } from './registro-model';
 
 @Component({
   selector: 'page-criar-regisros',
@@ -18,19 +19,59 @@ export class CriarRegistros {
   
   submitAttempt: boolean = false;
   
-  constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder) { 
+  constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder, public storage: Storage) { 
     
     //this.dataHoje = new Date().toISOString();
     
     this.myRegistro = formBuilder.group({
       nome: ['', Validators.compose([Validators.required])],
+      data: [this.dataHoje = new Date().toISOString(), Validators.compose([Validators.required])],
       //lastName: ['value', *validation function goes here*],
       //age: ['value', *validation function goes here*, *asynchronous validation function goes here*]
-      data: [this.dataHoje = new Date().toISOString(), Validators.compose([Validators.required])]
     });
     
+    // set a key/value
+    //storage.set('name', 'Max');
+    //storage.set('nome', this.myRegistro.get('nome').value);
+    
+     // Or to get a key/value pair
+    /*storage.get('name').then((val) => {
+      console.log('Your name is', val);
+    });*/
+  
   }
   
-  save(){}
+  //setData(){}
+    
+  //getData(){}
+  
+  save(){
+    
+    this.storage.set('nome', this.myRegistro.get('nome').value);
+    this.storage.set('data', this.myRegistro.get('data').value);
+    console.log('Saved');
+    /*this.submitAttempt = true;
+ 
+    if(!this.slideOneForm.valid){
+        this.signupSlider.slideTo(0);
+    } 
+    else if(!this.slideTwoForm.valid){
+        this.signupSlider.slideTo(1);
+    }
+    else {
+        console.log("success!")
+        console.log(this.slideOneForm.value);
+        console.log(this.slideTwoForm.value);
+    }*/
+  }
+  
+  show(){
+    this.storage.get('nome').then((val) => {
+      console.log('Your name is', val);
+    });
+    this.storage.get('data').then((val) => {
+      console.log('And your data is', val);
+    });
+  }
 
 }
